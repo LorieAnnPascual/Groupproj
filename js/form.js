eabc3a9003ddaef04dd884a54950518ce0d76030
Lorie Ann Pascual <lorie.ann.b.pascual@gmail.com>
2021-11-29 00:47:09 +0800

    deleted arrow errors

---
+++ js/form.js
@@ -0,0 +1,35 @@
+//Validation form
+// function sendData() 
+// {
+//     const myForm = document.forms["contactForm"];
+//     const fName = myForm["fname"].value.trim();
+   /*  const lName = myForm["lname"].value.trim();
+    const emailAdd = myForm["email"].value.trim();
+    const telNum = myForm["phone"].value.trim();
+    const services = document.querySelectorAll("#webDesign :checked");
+    const selBox = [...services].map(sel => sel.value); */
+    // console.log(fName);
+
+    /* if(fName =="") {
+        alert("Please enter your first name");
+        myForm["fname"].focus();
+        return false;
+    } else if (!isNaN(fName)) {
+        alert("Name should be in character.");
+        myForm["firstname"].focus();
+        return false;
+    } else if (lName =="") {
+        alert("Please type your last name.");
+        myForm["lastname"].focus();
+        return false;
+    } else if(emailAdd =="") {
+        alert("Please enter your email address");
+        myForm["email"].focus();
+        return false;
+    } else if(telNum =="") {
+        alert("Please enter your cellphone number");
+        myForm["phone"].focus();
+        return false;
+    } else if(telNum.length < 11) {
+        alert("Cellphone number must be ")
+    } */
