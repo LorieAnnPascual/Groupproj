863be7a267795e7238a147fbd1ce8f40b6cd49ea
Lorie Ann Pascual <lorie.ann.b.pascual@gmail.com>
2021-11-29 00:49:46 +0800

    deleted html errors

--- js/form.js
+++ js/form.js
@@ -1,16 +1,15 @@
 //Validation form
-// function sendData() 
-// {
-//     const myForm = document.forms["contactForm"];
-//     const fName = myForm["fname"].value.trim();
-   /*  const lName = myForm["lname"].value.trim();
+function sendData() 
+{
+    const myForm = document.forms["contactForm"];
+    const fName = myForm["fname"].value.trim();
+    const lName = myForm["lname"].value.trim();
     const emailAdd = myForm["email"].value.trim();
     const telNum = myForm["phone"].value.trim();
-    const services = document.querySelectorAll("#webDesign :checked");
-    const selBox = [...services].map(sel => sel.value); */
-    // console.log(fName);
+    const services = document.getElementById("box");
+    const textBox = myForm["message"].value.trim();
 
-    /* if(fName =="") {
+    if(fName =="") {
         alert("Please enter your first name");
         myForm["fname"].focus();
         return false;
@@ -31,5 +30,38 @@
         myForm["phone"].focus();
         return false;
     } else if(telNum.length < 11) {
-        alert("Cellphone number must be ")
-    } */
+        alert("Phone number should be maximum of 11 digits")
+        return false;
+    } else if(isNaN(telNum)) {
+        alert("Contact number should be in digits.");
+        myForm["phone"].focus();
+        return false;
+    } else if (!services.checked) {
+        alert("Select at least one service");
+        return false;
+    }else if(textBox =="") {
+        alert("Please type a brief message");
+        return false;
+    }  else {
+        confirm("You are about to send your message. Do you want to continue?")
+    }
+    
+}
+
+const cForm = document.forms["contactForm"];
+document.getElementById("btn").addEventListener("click", save);  
+const save=(event)=> {
+    event.preventDefault();
+    let obj = {
+        id: Date.now(),
+        // firstname: cForm["fname"].value,
+        // lastname: cForm["lname"],
+        // email: cForm["emailAdd"],
+        // tel: cForm["phone"],
+        // service: document.getElementById("#box"),
+        // message: cForm["message"]
+    }
+    console.log(JSON.stringify(obj, null, 2));
+    localStorage.setItem("cForm", JSON.stringify(obj));
+}
+
