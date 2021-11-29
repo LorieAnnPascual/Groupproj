3e47983a7ae260f721fb1821370cf3f9129841e3
Lorie Ann Pascual <lorie.ann.b.pascual@gmail.com>
2021-11-30 01:37:56 +0800

    edited layout, animation and form validation

--- js/form.js
+++ js/form.js
@@ -1,67 +1,71 @@
-//Validation form
-function sendData() 
+//Validation and saving to local storage
+const Submit = document.getElementById("btn");
+Submit.addEventListener("click",() => 
 {
-    const myForm = document.forms["contactForm"];
-    const fName = myForm["fname"].value.trim();
-    const lName = myForm["lname"].value.trim();
-    const emailAdd = myForm["email"].value.trim();
-    const telNum = myForm["phone"].value.trim();
-    const services = document.getElementById("box");
-    const textBox = myForm["message"].value.trim();
+    clientForm();
+})
+   let Data = localStorage.getItem('clientInformation')
+  ? JSON.parse(localStorage.getItem('clientInformation'))
+  : [];
+   formData = [];
+   
+   clientForm = () => 
+   {
+       const text1 = document.getElementById("fName").value;
+       const text2 = document.getElementById("lName").value;
+       const emailAdd = document.getElementById("email").value;
+       const Num = document.getElementById("phone").value;
 
-    if(fName =="") {
-        alert("Please enter your first name");
-        myForm["fname"].focus();
-        return false;
-    } else if (!isNaN(fName)) {
-        alert("Name should be in character.");
-        myForm["firstname"].focus();
-        return false;
-    } else if (lName =="") {
-        alert("Please type your last name.");
-        myForm["lastname"].focus();
-        return false;
-    } else if(emailAdd =="") {
-        alert("Please enter your email address");
-        myForm["email"].focus();
-        return false;
-    } else if(telNum =="") {
-        alert("Please enter your cellphone number");
-        myForm["phone"].focus();
-        return false;
-    } else if(telNum.length < 11) {
-        alert("Phone number should be maximum of 11 digits")
-        return false;
-    } else if(isNaN(telNum)) {
-        alert("Contact number should be in digits.");
-        myForm["phone"].focus();
-        return false;
-    } else if (!services.checked) {
-        alert("Select at least one service");
-        return false;
-    }else if(textBox =="") {
-        alert("Please type a brief message");
-        return false;
-    }  else {
-        confirm("You are about to send your message. Do you want to continue?")
-    }
-    
-}
+       if(text1 =="") {
+           alert("First name is required.");
+           contactForm.fName.focus();
+           return false;
+       }
+       if(text2 =="") {
+           alert("Last name is required.");
+           contactForm.lName.focus();
+           return false;
+       }
+       if(emailAdd =="") {
+           alert("Email is required.");
+           contactForm.email.focus();
+           return false;
+       }
+       if(Num =="") {
+           alert("Phone number is required.");
+           contactForm.phone.focus();
+           return false;
+       } else if(Num.length < 11) {
+           alert("Phone number has a minimum of 11 digits.");
+           contactForm.phone.focus();
+           return false;
+       }
 
-const cForm = document.forms["contactForm"];
-document.getElementById("btn").addEventListener("click", save);  
-const save=(event)=> {
-    event.preventDefault();
-    let obj = {
-        id: Date.now(),
-        // firstname: cForm["fname"].value,
-        // lastname: cForm["lname"],
-        // email: cForm["emailAdd"],
-        // tel: cForm["phone"],
-        // service: document.getElementById("#box"),
-        // message: cForm["message"]
-    }
-    console.log(JSON.stringify(obj, null, 2));
-    localStorage.setItem("cForm", JSON.stringify(obj));
-}
+       let confirmation = confirm("Are you sure you want to submit this form?");
+       if (confirmation) {
+           let fname = document.getElementById("fName").value;
+           let lname = document.getElementById("lName").value;
+           let email = document.getElementById("email").value;
+           let telNum = document.getElementById("phone").value;
+           let service = document.querySelectorAll("input[name='checkbox']:checked");
+           let selected = [...service].map(checked => checked.value);
+           let message = document.getElementById("message").value;
+
+           let client = {
+               id: Date.now(),
+               firstname: fname,
+               lastname: lname,
+               email: email,
+               telNum: telNum,
+               service: selected,
+               message: message
+           }
+           formData.push(client);
+           console.log(JSON.stringify(client, null, 2));
+           localStorage.setItem("client", JSON.stringify(formData));
+           document.contactForm.reset();
+           alert("Form submitted.");
+       } 
+       return(confirmation);
+   }
 
